d4ad1a6cc9f7e6579fff6b0f412da8cf051c5c3e
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-07 10:56:47 +0900

    ブログ及びお問い合わせ機能(index, new, create)を作成

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,23 @@
+class ContactsController < ApplicationController
+  def index
+    @contact = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to contacts_path, notice: '投稿しました'
+    else
+      render action: :new
+    end
+  end
+
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
