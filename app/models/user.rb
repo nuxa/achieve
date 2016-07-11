83510c9877862e7308069efd2ac97f2699a6309c
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-11 22:57:26 +0900

    validation外し

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email,
-    presence: true,
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }
-  validates :password,
-    presence: true
-  validates :password_confirmation,
-    presence: true
 end
