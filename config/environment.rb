7432b43775085799b92960f4d2c21d0aae7ebf37
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-12 19:33:11 +0900

    再びenv.rbからproduction.rbへ

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = 
-{
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['PASSWORD_USERNAME'],
-  :domain => "heroku.com",
-  :address => "smtp.sendgrid.net",
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { host: 'dive-into-code-achieve-eh0709.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['PASSWORD_USERNAME'],
+    :domain         => "heroku.com",
+    :address        => "smtp.sendgrid.net",
+    :port           => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
