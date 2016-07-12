a769c6a7f53dcf2f040d7e5781a949c81db75594
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-12 18:58:30 +0900

    config/enviroment/production.rbからconfig/enviroment.rbへsendgrid情報を移動

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,17 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
   config.action_mailer.default_url_options = { host: 'dive-into-code-achieve-eh0709' }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = 
-  {
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['PASSWORD_USERNAME'],
-    domain: 'heroku.com',
-    address: 'smtp.sendgrid.net',
-    port: 587,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
