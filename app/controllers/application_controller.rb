a19ef327fb9fbb3eddc5752c3106f196442700eb
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-14 13:10:58 +0900

    DIVE14テキスト完了状態（バグだらけ）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,11 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+    end
 end
