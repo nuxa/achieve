0fdcf1f5fd64c1254b0c114096e5652c8e16a40f
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-14 18:12:18 +0900

    controller修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,25 +6,19 @@ class ApplicationController < ActionController::Base
   # before_actionでdeviseのストロングパラメーターにnameカラムを追加するメソッドを実行する
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-  # deviseで使用するストロングパラメーターに項目を追加するメソッドです。
-  def configure_permitted_parameters
-    # サインアップ時にnameカラムを許容するようにします。
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント更新時にnameカラムを許容するようにします。
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to main_app.root_url, :alert => exception.message
   end
 
-  PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
-
   private
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
-      devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
-    end
+  # deviseで使用するストロングパラメーターに項目を追加するメソッドです。
+  def configure_permitted_parameters
+    # サインアップ時にnameカラムを許容するようにします。
+    devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
+    # アカウント更新時にnameカラムを許容するようにします。
+    devise_parameter_sanitizer.permit(:account_update, PERMISSIBLE_ATTRIBUTES)
+  end
 end
