856b3f4db9010846645339dcbbdd8f1eceafd129
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-14 22:54:16 +0900

    userをdestroyした際にblog#indexでエラーになってしまうので、user.rbにdependent: :destroy付加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable, :omniauthable
-  has_many :blogs
+  has_many :blogs, dependent: :destroy
   mount_uploader :avatar, AvatarUploader
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
