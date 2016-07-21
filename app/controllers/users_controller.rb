8f0d0a57d7cfa87508c3c8d99e211908834c2cf1
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-07-21 15:50:04 +0900

    DIVE16課題

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @users = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @followers = @user.followers
+    @followed_users = @user.followed_users
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 
   resources :relationships, only: [:create, :destroy]
 
