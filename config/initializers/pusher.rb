222ffae66e79e49f81203449d9eafa026208bd4f
eiji <holy.edge.nuxaxtuxtu@gmail.com>
2016-08-04 16:36:01 +0900

    pusherにalert追加、herokuてすと

--- config/initializers/pusher.rb
+++
@@ -1,6 +0,0 @@
-require 'pusher'
-Pusher.app_id = ENV['PUSHER_APP_ID']
-Pusher.key = ENV['PUSHER_ACCESS_KEY']
-Pusher.secret = ENV['PUSHER_ACCESS_KEY_SECRET']
-Pusher.logger = ENV['PUSHER_APP_ID']
-Pusher.encrypted = true
